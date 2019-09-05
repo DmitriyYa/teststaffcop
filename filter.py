af73e448c1c91de80bda3085e17469a708b51ec2
Dima Home <dmitriy.m.yakovlev@gmail.com>
2019-09-05 21:48:03 +0700

    Выделил действия в отдельные методы

---
+++ filter.py
@@ -0,0 +1,4 @@
+class Filter:
+    def __init__(self, title, list_emails_send):
+        self.title = title
+        self.list_emails_send = list_emails_send
--- test_add_filter.py
+++ test_add_filter.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from filter import Filter
 
 import unittest
 
@@ -9,30 +10,49 @@ class TestAddFilter(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.wd.maximize_window()
-    
-    def test_add_filter(self):
-        wd = self.wd
-        #login
-        wd.get("http://localhost/accounts/login/?next=/")
-        wd.find_element_by_id("id_username").send_keys("admin")
-        wd.find_element_by_id("id_password").send_keys("123")
-        wd.find_element_by_css_selector('input[type="submit"]').click()
 
-        #add filter
+    def create_group(self, wd, filter):
+        #init filter creation
         wd.find_element_by_css_selector('a[title="Create"]').click()
         wd.find_element_by_css_selector('.open > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click()
-        wd.find_element_by_css_selector('.input-xlarge').send_keys('Myfilter')
+        #fill filter form
+        wd.find_element_by_css_selector('.input-xlarge').clear()
+        wd.find_element_by_css_selector('.input-xlarge').send_keys(filter.title)
         wd.find_element_by_css_selector('.nav-tabs > li:nth-child(2) > a:nth-child(1)').click()
+        wd.find_element_by_css_selector('#id_emails').send_keys(filter.list_emails_send)
         wd.find_element_by_css_selector('.nav-tabs > li:nth-child(3) > a:nth-child(1)').click()
+        #submit filter creation
         wd.find_element_by_css_selector('button.btn-success:nth-child(2)').click()
 
-        #logout
+    def logout(self, wd):
         wd.find_element_by_css_selector('li.dropdown:nth-child(7) > a:nth-child(1)').click()
-        wd.find_element_by_css_selector('li.dropdown:nth-child(7) > ul:nth-child(2) > li:nth-child(8) > a:nth-child(1)').click()
+        wd.find_element_by_css_selector(
+            'li.dropdown:nth-child(7) > ul:nth-child(2) > li:nth-child(8) > a:nth-child(1)').click()
 
-    
+    def login(self, wd, username, password):
+        wd.find_element_by_id("id_username").send_keys(username)
+        wd.find_element_by_id("id_password").send_keys(password)
+        wd.find_element_by_css_selector('input[type="submit"]').click()
+
+    def open_home_page(self, wd):
+        wd.get("http://192.168.100.3/accounts/login/?next=/")
+
     def tearDown(self):
         self.wd.quit()
+
+    def test_add_filter(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "123")
+        self.create_group(wd, Filter("Myfilter", "asdasdasd"))
+        self.logout(wd)
+
+    # def test_add_empty_filter(self):
+    #     wd = self.wd
+    #     self.open_home_page(wd)
+    #     self.login(wd, username="admin", password="123")
+    #     self.create_group(wd, namefilter='')
+    #     self.logout(wd)
 
 
 if __name__ == "__main__":
